5cf805d0eeeace2d89da313ffb9ff516188bcfd7
TroyQuigg <troyquigg@gmail.com>
2026-02-17 23:01:15 -0800

    Swap and Rotate

--- Assets/Scripts/GameBoardManager.cs
+++ Assets/Scripts/GameBoardManager.cs
@@ -11,72 +11,51 @@ public class GameBoardManager : MonoBehaviour
 {
 
     [SerializeField] private List<PieceData> GameBoardPieceList;
-    //[SerializeField] GameObject testGameObject;
-    //[SerializeField] private TrianglePieceManager MyTrianglePieceManager;
 
     private int TotalGamePieces;
-
     private float RotationDelta;
-    private float DeltaX;
-    private float DeltaY;
     
     // Start is called before the first frame update
     void Start()
     {
-        TotalGamePieces = 32;
-
+        TotalGamePieces = 36;
         RotationDelta = 120f;
 
-        DeltaX = 10f;
-        DeltaY = 10f;
-
         SetupBoardPieces();
-
-        //MyTrianglePieceManager = testGameObject.GetComponent<TrianglePieceManager>();
-
-        //MyTrianglePieceManager.SetPieceType(GetRandomPieceType());
-        //MyTrianglePieceManager.SetPieceColors(GetRandomPieceColor(), GetRandomPieceColor(), GetRandomPieceColor());
-        //MyTrianglePieceManager.SetBackgroundColors(PieceColor.Black);
-        //MyTrianglePieceManager.SetOutlineColor(PieceColor.Black);
-        //MyTrianglePieceManager.OutlineOff();
-        //MyTrianglePieceManager.SetGlowColor(GetRandomPieceColor());
-        //MyTrianglePieceManager.GlowOn();
-
-
+        RandomRotation();
+        RandomMovement();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
     }
 
-    IEnumerator LerpMoveOverTime(GameObject a_GameObject, PieceData a_PieceData, float a_Duration)
+    private IEnumerator LerpMoveOverTime(PieceData a_GamePieceData, float a_Duration)
     {
-        a_PieceData.IsMoving = true;
+        a_GamePieceData.IsMoving = true;
         for (float t = 0f; t < a_Duration; t += Time.deltaTime)
         {
-            a_GameObject.transform.position = Vector3.Lerp(a_PieceData.CurrentPosition, a_PieceData.NewPosition, t / a_Duration);
+            a_GamePieceData.TrianglePieceObject.transform.position = Vector3.Lerp(a_GamePieceData.CurrentPosition, a_GamePieceData.NewPosition, t / a_Duration);
             yield return 0;
         }
-        a_GameObject.transform.position = a_PieceData.NewPosition;
-        a_PieceData.CurrentPosition = a_GameObject.transform.position;
-        a_PieceData.IsMoving = false;
+        a_GamePieceData.TrianglePieceObject.transform.position = a_GamePieceData.NewPosition;
+        a_GamePieceData.CurrentPosition = a_GamePieceData.TrianglePieceObject.transform.position;
+        a_GamePieceData.IsMoving = false;
     }
 
-    IEnumerator LerpRotateOverTime(GameObject a_GameObject, PieceData a_PieceData, float a_Duration)
+    private IEnumerator LerpRotateOverTime(PieceData a_GamePieceData, float a_Duration)
     {
-        a_PieceData.IsRotating = true;
-
+        a_GamePieceData.IsRotating = true;
         for (float t = 0f; t < a_Duration; t += Time.deltaTime)
         {
-            a_GameObject.transform.rotation = Quaternion.Lerp(a_PieceData.CurrentRotation, a_PieceData.NewRotation, t / a_Duration);
+            a_GamePieceData.TrianglePieceObject.transform.rotation = Quaternion.Lerp(a_GamePieceData.CurrentRotation, a_GamePieceData.NewRotation, t / a_Duration);
             yield return 0;
         }
-        a_GameObject.transform.rotation = a_PieceData.NewRotation;
-        a_PieceData.CurrentRotation = a_GameObject.transform.rotation;
-        a_PieceData.IsRotating = false;
+        a_GamePieceData.TrianglePieceObject.transform.rotation = a_GamePieceData.NewRotation;
+        a_GamePieceData.CurrentRotation = a_GamePieceData.TrianglePieceObject.transform.rotation;
+        a_GamePieceData.IsRotating = false;
     }
 
     private PieceColor GetRandomPieceColor()
@@ -103,22 +82,21 @@ public class GameBoardManager : MonoBehaviour
         _FullName.Append("TriangleGamePiece_");
 
         switch (a_Number) {
-            case int i when i >= 0 && i <=7:
+            case int i when i >= 0 && i <=8:
                 _FullName.Append("A0" + (a_Number + 1).ToString());
                 break;
-            case int i when i >= 8 && a_Number <= 15:
-                _FullName.Append("B0" + (a_Number - 7).ToString());
+            case int i when i >= 9 && a_Number <= 17:
+                _FullName.Append("B0" + (a_Number - 8).ToString());
                 break;
-            case int i when i >= 16 && a_Number <= 23:
-                _FullName.Append("C0" + (a_Number - 15).ToString());
+            case int i when i >= 18 && a_Number <= 26:
+                _FullName.Append("C0" + (a_Number - 17).ToString());
                 break;
-            case int i when i >= 24 && a_Number <= 31:
-                _FullName.Append("D0" + (a_Number - 23).ToString());
+            case int i when i >= 27 && a_Number <= 35:
+                _FullName.Append("D0" + (a_Number - 26).ToString());
                 break;
         }
         
         return _FullName.ToString();
-    
     }
 
     private void SetupBoardPieces()
@@ -133,8 +111,6 @@ public class GameBoardManager : MonoBehaviour
 
         foreach (PieceData GamePiece in GameBoardPieceList)
         {
-            float _Speed = UnityEngine.Random.Range(1, 5);
-
             TrianglePieceManager _TrianglePieceManager = GamePiece.TrianglePieceObject.GetComponent<TrianglePieceManager>();
 
             _TrianglePieceManager.SetPieceType(GetRandomPieceType());
@@ -144,16 +120,55 @@ public class GameBoardManager : MonoBehaviour
             _TrianglePieceManager.OutlineOff();
             _TrianglePieceManager.SetGlowColor(GetRandomPieceColor());
             _TrianglePieceManager.GlowOn();
-
-            GamePiece.NewPosition = new Vector3(GamePiece.TrianglePieceObject.transform.position.x + DeltaX, GamePiece.TrianglePieceObject.transform.position.y + DeltaY, GamePiece.TrianglePieceObject.transform.position.z);
-            GamePiece.IsMoving = false;
-            StartCoroutine(LerpMoveOverTime(GamePiece.TrianglePieceObject, GamePiece, _Speed));
-
-            GamePiece.NewRotation = Quaternion.Euler(0, 0, GamePiece.TrianglePieceObject.transform.rotation.eulerAngles.z + RotationDelta); ;
-            GamePiece.IsRotating = false;
-            StartCoroutine(LerpRotateOverTime(GamePiece.TrianglePieceObject, GamePiece, _Speed));
-
         }
+    }
+
+    private void RandomMovement()
+    {
+        for (int j = 0; j < GameBoardPieceList.Count; j++)
+        {
+            for (int i = 0; i < TotalGamePieces; i++)
+            {
+                int _PieceIndex1 = UnityEngine.Random.Range(0, 32);
+                int _PieceIndex2 = UnityEngine.Random.Range(0, 32);
+
+                if (_PieceIndex1 != _PieceIndex2)
+                {
+                    if (GameBoardPieceList[_PieceIndex1].CurrentRotation == GameBoardPieceList[_PieceIndex2].CurrentRotation)
+                    {
+                        if ((GameBoardPieceList[_PieceIndex1].IsMoving == false) && (GameBoardPieceList[_PieceIndex2].IsMoving == false))
+                        {
+                            GameBoardPieceList[_PieceIndex1].IsMoving = true;
+                            GameBoardPieceList[_PieceIndex2].IsMoving = true;
+                            SwapPiece(GameBoardPieceList[_PieceIndex1], GameBoardPieceList[_PieceIndex2], UnityEngine.Random.Range(1, 5));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void RandomRotation()
+    {
+        for (int i = 0; i < TotalGamePieces; i++)
+        {
+            RotatePiece(GameBoardPieceList[i], RotationDelta, UnityEngine.Random.Range(1, 5));
+        }
+    }
+
+    private void SwapPiece(PieceData a_GamePiece1, PieceData a_GamePiece2, float a_Speed)
+    {
+        a_GamePiece1.NewPosition = a_GamePiece2.CurrentPosition;
+        a_GamePiece2.NewPosition = a_GamePiece1.CurrentPosition;
+
+        StartCoroutine(LerpMoveOverTime(a_GamePiece1, a_Speed));
+        StartCoroutine(LerpMoveOverTime(a_GamePiece2, a_Speed));
+    }
+
+    private void RotatePiece(PieceData a_GamePiece, float a_RotateAmount, float a_Speed)
+    {
+        a_GamePiece.NewRotation = Quaternion.Euler(0, 0, a_GamePiece.TrianglePieceObject.transform.rotation.eulerAngles.z + a_RotateAmount);
+        StartCoroutine(LerpRotateOverTime(a_GamePiece, a_Speed));
     }
 
 }
@@ -185,6 +200,5 @@ public class PieceData
         IsRotating = false;
         TrianglePieceObject = a_TrianglePieceObject;
     }
-
 
 }
